ce877e248fc8c033e37a28fccc9f34f5a6498c78
Jason Kautz <jkautz@gmail.com>
2020-04-02 18:06:19 -0700

    removed schema.sql install from app_a rpm

--- SOURCES/app_a-1/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-  id integer primary key autoincrement,
-  username text not null,
-  password text not null,
-  token text not null);
-
-INSERT INTO users (username, password, token) VALUES ('density', 'password123', 'mytoken');
